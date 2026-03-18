c3e2232c65953f3a80c177a660e0a9ca979c6959
takurot <40074491+takurot@users.noreply.github.com>
2026-03-18 11:05:00 +0000

    chore(release): update package managers for v0.1.17

--- Formula/pybun.rb
+++ Formula/pybun.rb
@@ -12,20 +12,20 @@ class Pybun < Formula
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.17/pybun-aarch64-apple-darwin.tar.gz"
-        sha256 "ab3f7c49ce2fdda3570ddeaa4cc54f56aa64c1df376a35deb08e027b961ace62"
+        sha256 "72979bab9c40b1a8a8ee001ce5d63c36fedd8a5c5310c5e13a5011f233a312f6"
       else
         url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.17/pybun-x86_64-apple-darwin.tar.gz"
-        sha256 "351aa0f5190df32a894501524d665660b737eed5bc4abf4f1269a65d0554188e"
+        sha256 "1efdf061e00d8d7fa30150fa3d942b696572e6c0ceeacc1e7b1b6b0ea167a4aa"
       end
     end
 
     on_linux do
       if Hardware::CPU.arm?
         url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.17/pybun-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "3e8fb48cfe2ae8a3ff3434290c7e9c61ad0ca26e9a4097e06a79fd7f8fe5f297"
+        sha256 "bc00b7b7b3993ea259579450bb9386a6a113863324b39f99cc81714c7f62aba4"
       else
         url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.17/pybun-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "d428d883fc47cdab5700d04baaa48b9eea21dbaf1910730ef3ea0dc264ed6af0"
+        sha256 "870fa2b52619c99575aa7fc1efd97e706c4260eb2f2aa8858275a30a45bae9cb"
       end
     end
   end
