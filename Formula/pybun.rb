572b85ee2957f6860c4fdfd6ce27c2bed0fe0ac4
takurot <40074491+takurot@users.noreply.github.com>
2026-09-11 01:45:19 +0000

    chore(release): update package managers for v0.1.25

--- Formula/pybun.rb
+++ Formula/pybun.rb
@@ -2,7 +2,7 @@
 class Pybun < Formula
   desc "Rust-based single-binary Python toolchain."
   homepage "https://github.com/VOID-TECHNOLOGY-INC/PyBun"
-  version "0.1.24"
+  version "0.1.25"
   license "MIT"
 
   if ENV["HOMEBREW_PYBUN_TEST_TARBALL"]
@@ -11,21 +11,21 @@ class Pybun < Formula
   else
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.24/pybun-aarch64-apple-darwin.tar.gz"
-        sha256 "0e863d5b6a876ba6d7d6f47e4c47fe35f8e2a5d9643d4ffb5e4a6cba4046e966"
+        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.25/pybun-aarch64-apple-darwin.tar.gz"
+        sha256 "534881ad0ce9c12d9ebbd2b183abad6617123efe5605c59f275bbb7f40b65a8d"
       else
-        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.24/pybun-x86_64-apple-darwin.tar.gz"
-        sha256 "9e5ebc17ba7c1ba9a4dc25a2c3790803c0ee496dd72c25803db48fbffa23c030"
+        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.25/pybun-x86_64-apple-darwin.tar.gz"
+        sha256 "b7cf6ee61085ea496e27dfbfac5a8b186aba1a317851ec74bff09312db059256"
       end
     end
 
     on_linux do
       if Hardware::CPU.arm?
-        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.24/pybun-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "78f02b33657e16c6242c1e5decb3c2f405be2dd4b8afb0b13efbedd7b4a6903c"
+        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.25/pybun-aarch64-unknown-linux-gnu.tar.gz"
+        sha256 "74d53a7951889366c0f7d3cc1c92cd9e630e3fb313c3c82c4f6a79382a0a65ac"
       else
-        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.24/pybun-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "855d6126d39fd210fe2b90a4da8d720245d39f0423e5b5cdb61abbc277029690"
+        url "https://github.com/VOID-TECHNOLOGY-INC/PyBun/releases/download/v0.1.25/pybun-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "1f0d0b328a5010e0bbf0a2dbed7ebf34e93d8977372891b92afe7fd5866e1dc7"
       end
     end
   end
